b8cbf6d604082f9913e9fc068317a7b5215116b2
bangnguyen1vs9 <huubang16042002@gmail.com>
2023-04-01 23:19:10 +0700

    Updating data for the 1st time

--- footer.php
+++ footer.php
@@ -5,6 +5,20 @@
                     <div class="grid__column-4">
                         <div class="footer-list">
                             <h2 class="sub-title">
+                                MixiShop
+                            </h2>
+                            <p class="desc">
+                            Lầu 5, 387-389 Hai Bà Trưng Quận 3 TP HCMCông Ty Cổ Phần Phát Hành Sách TP HCM - FAHASA60 - 62 Lê Lợi, Quận 1, TP. HCM, Việt Nam
+                            </p>
+                            <p class="desc">
+                            MixiShop.com nhận đặt hàng trực tuyến và giao hàng tận nơi. 
+                            KHÔNG hỗ trợ đặt mua và nhận hàng trực tiếp tại văn phòng cũng như tất cả Hệ Thống MixiShop trên toàn quốc.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="grid__column-4">
+                        <div class="footer-list">
+                            <h2 class="sub-title">
                                 VỀ CHÚNG TÔI
                             </h2>
                             <p class="desc">
@@ -15,15 +29,20 @@
                     <div class="grid__column-4">
                         <div class="footer-list">
                             <h2 class="sub-title">
-                                VỀ CHÚNG TÔI
+                                LIÊN HỆ
                             </h2>
-                        </div>
-                    </div>
-                    <div class="grid__column-4">
-                        <div class="footer-list">
-                            <h2 class="sub-title">
-                                THÔNG TIN
-                            </h2>
+                            <p class="desc">
+                            <i class="fa-sharp fa-solid fa-location-dot"></i>
+                                60-62 Lê Lợi, Q.1, TP. HCM
+                            </p>
+                            <p class="desc">
+                            <a href="tel:0865476228"><i class="fa-solid fa-phone"></i>
+                            0865476228</a>
+                            </p>
+                            <p class="desc">
+                            <a href="mailto:huubang16042002@gmail.com"><i class="fa-regular fa-envelope"></i> 
+                                 huubang16042002@gmail.com   </a>    
+                            </p>
                         </div>
                     </div>
                     <div class="grid__column-4">
